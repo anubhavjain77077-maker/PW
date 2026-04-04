fe8834465252fd0d5ac33d97a2823310bd7fe83a
Anubhav Jain <anubhavjain77077@gmail.com>
2026-04-04 13:00:45 +0800

    Initial commit - Playwright framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
     ['html', { open: 'never' }],
+    ['junit', { outputFile: 'test-results/results.xml' }],
     ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
